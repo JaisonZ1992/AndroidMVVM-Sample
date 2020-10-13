41fe94c11605118b2f2f905960d4c3c9c1fea49c
Jaison Zachariah <zachariah.jaison@gmail.com>
2020-10-14 01:27:36 +0530

    RX java splash screen

--- app/src/main/java/com/example/myapplication/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/splash/SplashActivity.kt
@@ -2,28 +2,41 @@ package com.example.myapplication.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
-import com.example.myapplication.ui.post.PostListActivity
+import com.example.myapplication.ui.login.LoginActivity
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import java.util.concurrent.TimeUnit
 
 
 class SplashActivity : AppCompatActivity() {
-    private val INTERVAL = 1500L;
+    private val INTERVAL = 2L
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startApplication()
+        startMainActivityUsingRxJava()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        compositeDisposable.dispose()
+    }
+
+    private fun startMainActivityUsingRxJava() {
+        compositeDisposable.add(
+            Observable.timer(INTERVAL, TimeUnit.SECONDS)
+                .subscribe {
+                    startApplication()
+                })
     }
 
     private fun startApplication() {
-        Handler(Looper.myLooper()!!).postDelayed(Runnable {
-            val intent = Intent(this@SplashActivity, PostListActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        }, INTERVAL);
+        val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
 }
